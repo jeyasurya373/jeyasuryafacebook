355d3b842c53d18066206dc341d4a3a7d95aaaf1
jeyasurya373 <84000088+jeyasurya373@users.noreply.github.com>
2021-11-15 12:19:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+
+const app = express();
+
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+
+const helmet = require("helmet");
+
+const morgan = require("morgan");
+
+const multer = require("multer");
+
+const path = require("path");
+
+const cors = require("cors");
+
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("connected to mongo")
+});
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+app.use(express.json());
+
+app.use(helmet());
+
+app.use(morgan("common"));
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("facebook connected successfully")
+})
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+        return res.status(200).json("file uploaded successfully")
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+
+
+app.listen(process.env.PORT || 8800, () => {
+    console.log("backend api running")
+})
